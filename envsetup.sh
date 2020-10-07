07a5c4020f059f4d946c13e3a437758880393aa3
AgentFabulous <kshitijgm@gmail.com>
2020-10-07 07:06:45 +0200

    build: Add TPP ASCII art on lunch
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -550,6 +550,10 @@ function _lunch_meat()
     if [[ -n "${CHECK_MU_CONFIG:-}" ]]; then
       check_mu_config
     fi
+
+    echo "";
+    cat $(gettop)/build/make/custom_ascii_logo;
+    echo"";
 }
 
 unset COMMON_LUNCH_CHOICES_CACHE
